0ce24824e9589a1475aed2a18b7e5b925c61c022
soulbliss <deepak.garasangi@gmail.com>
2021-02-21 16:15:22 +0530

    bookmarks uuid, line, query with uuid more

--- src/components/Bookmark/Bookmark.js
+++ src/components/Bookmark/Bookmark.js
@@ -9,9 +9,11 @@ import withAuth from './../withAuth';
 
 const Bookmark = ({ session }) => {
 
-    const [booksList, setBooksList] = useState([])
+    const [uuidList, setUuidsList] = useState([])
     const [queryList, setQueryList] = useState([])
     const [linesList, setLinesList] = useState([])
+
+    const [dict, setDict] = useState(false)
 
     let bookmarkedFilesPoint = 'http://127.0.0.1:4444/api/v2/bookmarks'
 
@@ -29,47 +31,38 @@ const Bookmark = ({ session }) => {
             'authorization': token
         }
     }
+    let axiosPayload = {
 
+        "start": 0,
 
-    useEffect(() => {
-
-
-        setUp()
-
-    }, [])
-
-    const setUp = async () => {
-        
-        axios.get(
-            bookmarkedFilesPoint,
-            axiosConfig
-        )
-            .then(res => { setBooksList(res.data); return res.data })
-            .then(res => { /* list of uuids */  return res[0] })
-            .then(uuid => {
-                /* get query id */
-                return axios.get(bookmarkedFilesPoint + '/' + uuid, axiosConfig).then(queryIdData => { return [uuid, queryIdData.data[0]] })
-                
-            })
-            .then(uuidAndQuery=> {
-                /* get line id */
-            
-                return axios.get(bookmarkedFilesPoint + '/' + uuidAndQuery[0] + '/' + uuidAndQuery[1], axiosConfig)
-
-            }).then(res => {
-                /* line id present here */
-                console.log(res.data)
-            })
+        "end": 3
 
     }
 
 
+    useEffect(() => {
+        axios.post(
+            bookmarkedFilesPoint,
+            axiosConfig
+        )
+            .then(res => {
 
 
-    console.log(booksList, typeof (booksList))
+                setUuidsList(res.data['uuids'])
+                setQueryList(res.data['queries'])
+                setLinesList(res.data['lines'])
+
+                return res
+            }).then(res => setDict(true))
+
+    }, [])
+
+    console.log(uuidList)
     console.log(queryList)
-
     console.log(linesList)
+
+
+
     return (
 
         <Container>
@@ -82,27 +75,58 @@ const Bookmark = ({ session }) => {
 
                 </Col>
 
-                <Col md="9" style={{marginTop: 20}}>
+                <Col md="9" style={{ marginTop: 20 }}>
 
                     <h4>welcome to your bookmarks!</h4>
                     <h4>{username}</h4>
 
                     <hr />
 
-                    {/* {
+                    {dict === false ?
+
+                        <p>not loaded</p> :
+
                         queryList.map((queries, queriesIndex) => {
-                            return queries.data.map((query, queryIndex) => {
-                                if (queryIndex === 0) {
-                                    return <div> <h4>{booksList[queriesIndex]}</h4><p>{query}</p></div>
-                                } else if (queriesIndex === queries.length) {
-                                    return <div><p>{query}</p><hr /></div>
-                                } else {
-                                    return <div><p>{query}</p></div>
-                                }
-                            }
-                            )
+                            return queries.map((query, queryIndex) => {
+
+                                return linesList[queriesIndex][queryIndex].map((line, lineIndex) => {
+
+                                    if (queryIndex === 0 && lineIndex === 0) {
+                                        // query index and line index zero means under a new uuid
+                                        if (queriesIndex === queryList.length - 1 &&
+                                            queryIndex === queries.length - 1 &&
+                                            lineIndex === linesList[queriesIndex][queryIndex].length - 1
+                                        ) {
+                                            return <div> <h4>{uuidList[queriesIndex]}</h4><hr /> <p>{query}</p><p>{line}</p><h1>More</h1></div>
+                                        } else
+                                            return <div> <h4>{uuidList[queriesIndex]}</h4><hr /> <p>{query}</p><p>{line}</p></div>
+                                    } else if (lineIndex === 0) {
+                                        // line index zero means under a new query
+                                        if (queriesIndex === queryList.length - 1 &&
+                                            queryIndex === queries.length - 1 &&
+                                            lineIndex === linesList[queriesIndex][queryIndex].length - 1
+                                        ) {
+                                            return <div> <p>{query}</p><p>{line}</p><h1>More</h1></div>
+                                        } else
+                                            return <div><p>{query}</p><p>{line}</p></div>
+                                    } else if (queriesIndex === queryList.length - 1 &&
+                                        queryIndex === queries.length - 1 &&
+                                        lineIndex === linesList[queriesIndex][queryIndex].length - 1
+                                    ) {
+                                        return <div><p>{line}</p><h1>More</h1></div>
+                                    } else {
+                                        // arbitary line, so just display it
+
+                                        return <div><p>{line}</p></div>
+                                    }
+
+                                })
+
+                            })
                         })
-                    } */}
+                    }
+
+
 
                     <br />
 
